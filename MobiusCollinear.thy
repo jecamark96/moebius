025b5db000719149ceba27350dff0accdeed8060
jecamark96 <37874302+jecamark96@users.noreply.github.com>
2025-02-02 14:09:08 +0100

    Update MobiusCollinear.thy
    fix u nazivu

--- MobiusCollinear.thy
+++ MobiusCollinear.thy
@@ -1,19 +1,19 @@
 theory MobiusCollinear
-  imports MobiusGyroVectorSpace
+  imports MobiusGyroVectorSpace 
 begin
 
 lemma collinear_0_proportional':
-  assumes "v \<noteq> 0\<^sub>m"
-  shows "Mobius_gyrovector_space.collinear x 0\<^sub>m v \<longleftrightarrow> (\<exists> k::real. to_complex x = k * (to_complex v))"
-  unfolding Mobius_gyrovector_space.collinear_def gyroplus_PoincareDisc_def gyroinv_PoincareDisc_def
+  assumes "v ≠ 0⇩m"
+  shows "Mobius_pre_gyrovector_space.collinear x 0⇩m v ⟷ (∃ k::real. to_complex x = k * (to_complex v))"
+  unfolding Mobius_pre_gyrovector_space.collinear_def gyroplus_PoincareDisc_def gyroinv_PoincareDisc_def
   using assms
 proof transfer
   fix v x
-  assume "cmod v < 1" "cmod x < 1" "v \<noteq> ozero_m'"
-  then have "v \<noteq> 0"
+  assume "cmod v < 1" "cmod x < 1" "v ≠ ozero_m'"
+  then have "v ≠ 0"
     unfolding ozero_m'_def
     by simp
-  have "(\<exists>t. x = (otimes'_k t v) * v / cor (cmod v)) \<longleftrightarrow> (\<exists>k :: real. x = k * v)" (is "?lhs \<longleftrightarrow> ?rhs")
+  have "(∃t. x = (otimes'_k t v) * v / cor (cmod v)) ⟷ (∃k :: real. x = k * v)" (is "?lhs ⟷ ?rhs")
   proof
     assume ?lhs
     then show ?rhs
@@ -26,83 +26,83 @@ proof transfer
     moreover
 
     have "abs (k * cmod v) < 1"
-      by (metis \<open>cmod x < 1\<close> \<open>x = cor k * v\<close> abs_mult abs_norm_cancel norm_mult norm_of_real)
+      by (metis ‹cmod x < 1› ‹x = cor k * v› abs_mult abs_norm_cancel norm_mult norm_of_real)
 
-    have "artanh (cmod v) \<noteq> 0"
-      using \<open>v \<noteq> 0\<close>
-      by (simp add: \<open>cmod v < 1\<close> artanh_not_0)
+    have "artanh (cmod v) ≠ 0"
+      using ‹v ≠ 0›
+      by (simp add: ‹cmod v < 1› artanh_not_0)
     
-    have "\<exists> t. (otimes'_k t v) / (cmod v) = k"
+    have "∃ t. (otimes'_k t v) / (cmod v) = k"
     proof-
       let ?t = "artanh(k * cmod v) / artanh (cmod v)"
       have "tanh (?t * artanh (cmod v)) = k * cmod v"
-        using \<open>artanh (cmod v) \<noteq> 0\<close> tanh_artanh[of "k * cmod v"] \<open>abs (k * cmod v) < 1\<close>
+        using ‹artanh (cmod v) ≠ 0› tanh_artanh[of "k * cmod v"] ‹abs (k * cmod v) < 1›
         by (simp add: field_simps)
       then show ?thesis
-        by (metis \<open>cmod v < 1\<close> \<open>v \<noteq> 0\<close> nonzero_mult_div_cancel_right norm_zero otimes'_k_tanh zero_less_norm_iff)
+        by (metis ‹cmod v < 1› ‹v ≠ 0› nonzero_mult_div_cancel_right norm_zero otimes'_k_tanh zero_less_norm_iff)
     qed
     ultimately
     show ?lhs
       by auto
   qed
-  then show "(ozero_m' = v \<or> (\<exists>t. x = oplus_m' ozero_m' (otimes' t (oplus_m' (ominus_m' ozero_m') v)))) \<longleftrightarrow>
-        (\<exists> k::real. x = k * v)"
-    using \<open>v \<noteq> 0\<close>
+  then show "(ozero_m' = v ∨ (∃t. x = oplus_m' ozero_m' (otimes' t (oplus_m' (ominus_m' ozero_m') v)))) ⟷
+        (∃ k::real. x = k * v)"
+    using ‹v ≠ 0›
     unfolding oplus_m'_def ozero_m'_def ominus_m'_def otimes'_def
     by simp
 qed
 
 lemma
-  assumes "v \<noteq> 0\<^sub>m"
-  shows "Mobius_gyrovector_space.collinear x 0\<^sub>m v \<longleftrightarrow> to_complex x * cnj (to_complex v) = cnj (to_complex x) * to_complex v"
+  assumes "v ≠ 0⇩m"
+  shows "Mobius_pre_gyrovector_space.collinear x 0⇩m v ⟷ to_complex x * cnj (to_complex v) = cnj (to_complex x) * to_complex v"
   using Mobius_gyrocarrier'.to_carrier_zero_iff assms cnj_mix_ex_real_k collinear_0_proportional' gyrozero_PoincareDisc_def
   by fastforce
 
 lemma collinear_0_proportional:
-  shows "Mobius_gyrovector_space.collinear x 0\<^sub>m v \<longleftrightarrow> v = 0\<^sub>m \<or> (\<exists> k::real. to_complex x = k * (to_complex v))"
-  by (metis Mobius_gyrovector_space.collinear_def collinear_0_proportional')
+  shows "Mobius_pre_gyrovector_space.collinear x 0⇩m v ⟷ v = 0⇩m ∨ (∃ k::real. to_complex x = k * (to_complex v))"
+  by (metis Mobius_pre_gyrovector_space.collinear_def collinear_0_proportional')
 
 lemma to_complex_0 [simp]:
-  shows "to_complex 0\<^sub>m = 0"
+  shows "to_complex 0⇩m = 0"
   by transfer (simp add: ozero_m'_def)
 
 lemma to_complex_0_iff [iff]:
-  shows "to_complex x = 0 \<longleftrightarrow> x = 0\<^sub>m"
+  shows "to_complex x = 0 ⟷ x = 0⇩m"
   by transfer (simp add: ozero_m'_def)
 
 lemma mobius_between_0xy:
-  shows "Mobius_gyrovector_space.between 0\<^sub>m x y \<longleftrightarrow> 
-         (\<exists> k::real. 0 \<le> k \<and> k \<le> 1 \<and> to_complex x = k * to_complex y)"
-proof (cases "y = 0\<^sub>m")
+  shows "Mobius_pre_gyrovector_space.between 0⇩m x y ⟷ 
+         (∃ k::real. 0 ≤ k ∧ k ≤ 1 ∧ to_complex x = k * to_complex y)"
+proof (cases "y = 0⇩m")
   case True
   then show ?thesis
-    using Mobius_gyrovector_space.between_xyx[of "0\<^sub>m" x]
+    using Mobius_pre_gyrovector_space.between_xyx[of "0⇩m" x]
     by auto
 next
   case False
   then show ?thesis
-    unfolding Mobius_gyrovector_space.between_def gyroplus_PoincareDisc_def gyrozero_PoincareDisc_def gyroinv_PoincareDisc_def
+    unfolding Mobius_pre_gyrovector_space.between_def gyroplus_PoincareDisc_def gyrozero_PoincareDisc_def gyroinv_PoincareDisc_def
   proof (transfer)
     fix x y
-    assume "cmod y < 1" "y \<noteq> ozero_m'" "cmod x < 1"
-    then have "y \<noteq> 0"
+    assume "cmod y < 1" "y ≠ ozero_m'" "cmod x < 1"
+    then have "y ≠ 0"
       by (simp add: ozero_m'_def)
 
-    have "(\<exists>t\<ge>0. t \<le> 1 \<and> x = cor (otimes'_k t y) * y / cor (cmod y)) =
-          (\<exists>k\<ge>0. k \<le> 1 \<and> x = cor k * y)" (is "?lhs = ?rhs")
+    have "(∃t≥0. t ≤ 1 ∧ x = cor (otimes'_k t y) * y / cor (cmod y)) =
+          (∃k≥0. k ≤ 1 ∧ x = cor k * y)" (is "?lhs = ?rhs")
     proof
       assume ?lhs
-      then obtain t where "0 \<le> t" "t \<le> 1" "x = (otimes'_k t y / cmod y) * y"
+      then obtain t where "0 ≤ t" "t ≤ 1" "x = (otimes'_k t y / cmod y) * y"
         by auto
       moreover
-      have "0 \<le> otimes'_k t y / cmod y" 
-        unfolding otimes'_k_tanh[OF \<open>cmod y < 1\<close>]
-        using \<open>cmod y < 1\<close> \<open>t \<ge> 0\<close> tanh_artanh_nonneg 
+      have "0 ≤ otimes'_k t y / cmod y" 
+        unfolding otimes'_k_tanh[OF ‹cmod y < 1›]
+        using ‹cmod y < 1› ‹t ≥ 0› tanh_artanh_nonneg 
         by auto
       moreover
-      have "otimes'_k t y / cmod y \<le> 1"
-        unfolding otimes'_k_tanh[OF \<open>cmod y < 1\<close>]
-        using \<open>cmod y < 1\<close> \<open>y \<noteq> 0\<close> artanh_nonneg \<open>t \<le> 1\<close>
+      have "otimes'_k t y / cmod y ≤ 1"
+        unfolding otimes'_k_tanh[OF ‹cmod y < 1›]
+        using ‹cmod y < 1› ‹y ≠ 0› artanh_nonneg ‹t ≤ 1›
         by (smt (verit, best) divide_le_eq_1 mult_le_cancel_right2 norm_le_zero_iff strict_mono_less_eq tanh_artanh tanh_real_strict_mono)
       ultimately
       show ?rhs
@@ -115,37 +115,37 @@ next
       moreover
 
       have "abs (k * cmod y) < 1"
-        by (metis \<open>cmod x < 1\<close> \<open>x = cor k * y\<close> abs_mult abs_norm_cancel norm_mult norm_of_real)
+        by (metis ‹cmod x < 1› ‹x = cor k * y› abs_mult abs_norm_cancel norm_mult norm_of_real)
 
-      have "artanh (cmod y) \<noteq> 0"
-        using \<open>y \<noteq> 0\<close>
-        by (simp add: \<open>cmod y < 1\<close> artanh_not_0)
+      have "artanh (cmod y) ≠ 0"
+        using ‹y ≠ 0›
+        by (simp add: ‹cmod y < 1› artanh_not_0)
     
-      have "\<exists> t. 0 \<le> t \<and> t \<le> 1 \<and> (otimes'_k t y) / (cmod y) = k"
+      have "∃ t. 0 ≤ t ∧ t ≤ 1 ∧ (otimes'_k t y) / (cmod y) = k"
       proof-
         let ?t = "artanh(k * cmod y) / artanh (cmod y)"
         have "tanh (?t * artanh (cmod y)) = k * cmod y"
-          using \<open>artanh (cmod y) \<noteq> 0\<close> tanh_artanh[of "k * cmod y"] \<open>abs (k * cmod y) < 1\<close>
+          using ‹artanh (cmod y) ≠ 0› tanh_artanh[of "k * cmod y"] ‹abs (k * cmod y) < 1›
           by (simp add: field_simps)
         moreover
-        have "?t \<ge> 0"
-          using \<open>\<exists>k\<ge>0. k \<le> 1 \<and> x = cor k * y\<close> \<open>cmod y < 1\<close> \<open>x = cor k * y\<close> \<open>y \<noteq> 0\<close>
+        have "?t ≥ 0"
+          using ‹∃k≥0. k ≤ 1 ∧ x = cor k * y› ‹cmod y < 1› ‹x = cor k * y› ‹y ≠ 0›
           by (smt (verit, ccfv_SIG)  artanh_nonneg calculation divide_eq_0_iff mult_right_cancel norm_le_zero_iff of_real_eq_iff tanh_real_nonneg_iff zero_le_mult_iff)
         moreover
-        have "?t \<le> 1"
-          using \<open>\<exists>k\<ge>0. k \<le> 1 \<and> x = cor k * y\<close> \<open>cmod y < 1\<close> \<open>x = cor k * y\<close> \<open>y \<noteq> 0\<close>
+        have "?t ≤ 1"
+          using ‹∃k≥0. k ≤ 1 ∧ x = cor k * y› ‹cmod y < 1› ‹x = cor k * y› ‹y ≠ 0›
           by (smt (verit, ccfv_SIG)  artanh_monotone artanh_nonneg calculation(1) less_divide_eq_1 nonzero_divide_eq_eq of_real_eq_iff tanh_artanh_nonneg zero_less_norm_iff)
         ultimately show ?thesis
-          by (metis \<open>cmod y < 1\<close> \<open>y \<noteq> 0\<close> nonzero_mult_div_cancel_right norm_zero otimes'_k_tanh zero_less_norm_iff)
+          by (metis ‹cmod y < 1› ‹y ≠ 0› nonzero_mult_div_cancel_right norm_zero otimes'_k_tanh zero_less_norm_iff)
       qed
       ultimately
       show ?lhs
         by auto
     qed
-    then show "(\<exists>t\<ge>0. t \<le> 1 \<and>
+    then show "(∃t≥0. t ≤ 1 ∧
                        x = oplus_m' ozero_m' (otimes' t (oplus_m' (ominus_m' ozero_m') y))) =
-               (\<exists>k\<ge>0. k \<le> 1 \<and> x = cor k * y)"
-      using \<open>y \<noteq> 0\<close>
+               (∃k≥0. k ≤ 1 ∧ x = cor k * y)"
+      using ‹y ≠ 0›
       unfolding ozero_m'_def oplus_m'_def ominus_m'_def otimes'_def
       by simp
   qed
